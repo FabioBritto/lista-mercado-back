2d29f10c5472340fc0321f24a34d41266b8c2d28
FabioBritto <fabio.tritono@gmail.com>
2025-02-26 16:04:47 -0300

    Adicionada annotation para recebimento de requisições de outras origens

--- src/main/java/br/com/fabiobritto/applistamercado/controller/ListaController.java
+++ src/main/java/br/com/fabiobritto/applistamercado/controller/ListaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.fabiobritto.applistamercado.model.Lista;
 import br.com.fabiobritto.applistamercado.service.IListaService;
 
+/*
+ * @CrossOrigin("*") me permite receber requisições de outras origens
+ */
 @RestController
+@CrossOrigin("*")
 public class ListaController {
 
 	@Autowired
